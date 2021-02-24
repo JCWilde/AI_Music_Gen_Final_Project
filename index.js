94d0c0dd8615feee24d9ca66272445e7a2db73e4
jchild <jakobhild49@gmail.com>
2021-02-23 21:49:45 -0500

    added up to what I've had for a while

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const http = require("http");
-const fs = require('fs').promises;
+const fs = require("fs");
 
 const host = 'localhost';
 const port = '8000';
 
+
 const requestListener = function(req, res) {
-    fs.readFile(__dirname + "/template.html").then(contents => {
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end(contents);
-    }).catch(err => {
-        res.writeHead(200);
-        res.end(err);
-        return;
+    console.log(req.url);
+    if (req.url == "/") req.url = "/index.html";
+    fs.readFile("." + req.url, "UTF-8", function(err, file) {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end(err);
+        }
+        res.writeHead(200, { "Content-Type": "text/" + (req.url).split(".")[1] });
+        res.end(file);
     });
-};
+}
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log('server is running on http://${host}:${port}');
-});
+    console.log('server is running on http://' + host + ':' + port);
+})
