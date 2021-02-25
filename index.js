4eb7553a361cb77df0d5f4f02a9fa69acb97dce6
jchild <jakobhild49@gmail.com>
2021-02-25 10:37:58 -0500

    added redirect for invalid requests and spruced up coding

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.get('/style.css', function(req, res) {
-    res.sendFile(__dirname + "/style.css")
-})
+// return scripts like css and js
+app.get('/style.css', function(req, res) { res.sendFile(__dirname + "/style.css") })
+app.get('/js/midiInp.js', function(req, res) { res.sendFile(path.join(__dirname + "/js/midiInp.js")) })
 
-app.get('/js/midiInp.js', function(req, res) {
-    res.sendFile(path.join(__dirname + "/js/midiInp.js"))
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"))
-})
+// index get and post methods
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname + "/index.html")) })
 
 app.post('/', (req, res) => {
     console.log(req.body)
     res.sendFile(path.join(__dirname + "/index.html"))
 })
 
-app.get('/license', (req, res) => {
-    res.sendFile(path.join(__dirname + "/license.html"))
-})
+// license page
+app.get('/license', (req, res) => { res.sendFile(path.join(__dirname + "/license.html")) })
 
+// deal with all requests that are invalid, redirect to index
+app.get('*', (req, res) => { res.redirect('/') })
+
+// start listening
 app.listen(8000);
